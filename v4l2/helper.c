f34dacc6efea73c902fea0890aadffac86a0079d
Youngmin Kim <ymkim@huvitz.com>
2019-07-05 17:13:53 +0900

    play camera

---
+++ v4l2/helper.c
@@ -0,0 +1,40 @@
+#include "helper.h"
+
+static gboolean print_field(GQuark field, const GValue * value, gpointer pfx)
+{
+	gchar *str = gst_value_serialize(value);
+
+	g_print("%s  %15s: %s\n", (gchar *)pfx, g_quark_to_string(field), str);
+	g_free(str);
+
+	return TRUE;
+}
+
+
+void print_caps(const GstCaps * caps, const gchar *pfx)
+{
+	guint i;
+
+	g_return_if_fail(caps != NULL);
+
+	if ( gst_caps_is_any(caps) ) 
+	{
+		g_print("%sANY\n", pfx);
+		return;
+	}
+
+	if ( gst_caps_is_empty(caps) )
+	{
+		g_print("%sEMPTY\n", pfx);
+		return;
+	}
+
+	for ( i = 0 ; i < gst_caps_get_size(caps); i++ )
+	{
+		GstStructure * structure = gst_caps_get_structure(caps, i);
+
+		g_print("%s%s\n", pfx, gst_structure_get_name(structure));
+		gst_structure_foreach(structure, print_field, (gpointer)pfx);
+	}
+}
+/********** end of file **********/
---
+++ v4l2/helper.h
@@ -0,0 +1,19 @@
+/*!
+ * \file helper.h
+ * \brief
+ *
+ *
+ * created by Kim Youngmin (ymkim@huvitz.com)
+ * creation date : 2019/07/05 09:47:33 (KST)
+ *
+*/
+
+#ifndef _HELPER_H_
+#define _HELPER_H_
+
+#include <gst/gst.h>
+
+void print_caps(const GstCaps * caps, const gchar *pfx);
+
+#endif
+/********** end of file **********/
