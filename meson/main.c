f25414398e155146685ec6c22f30bb91399e5cc2
Youngmin Kim <kimyoungmin@gmail.com>
2020-04-14 17:57:08 +0900

    meson tutorial - gtk dep

--- meson/main.c
+++ meson/main.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
+#include <gtk/gtk.h>
+
 
 int main(int argc, char **argv)
 {
-	printf("Hello there.\n");
+	GtkWidget *win;
 
+	gtk_init(&argc, &argv);
+
+	win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(win), "Hello World");
+	g_signal_connect(win, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_widget_show(win);
+
+	gtk_main();
 	return 0;
 }
 
