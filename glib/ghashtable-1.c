4f67bccdb199ff62f93ca362c8603e18eb203f3d
Youngmin Kim <kimyoungmin@gmail.com>
2020-04-28 17:50:40 +0900

    glib GHashTable

---
+++ glib/ghashtable-1.c
@@ -0,0 +1,20 @@
+#include <glib.h>
+#include <stdio.h>
+
+int main(int argc, char*argv[])
+{
+	GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
+
+	g_hash_table_insert(hash, "Virginia", "Richmod");
+	g_hash_table_insert(hash, "Texas", "Austin");
+	g_hash_table_insert(hash, "Ohio", "Columbus");
+
+	printf("There are %d keys in the hash\n", g_hash_table_size(hash));
+	printf("The capital of Texas is %s\n", (char*)g_hash_table_lookup(hash, "Texas"));
+
+	gboolean found = g_hash_table_remove(hash, "Virginia");
+	printf("The value 'Virginia' was %sfound and removed\n", found ? "" : "not ");
+	g_hash_table_destroy(hash);
+
+	return 0;
+}
