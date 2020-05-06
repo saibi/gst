e17962ae1be3f059ed631fb9da9c05d2e774fe9e
Youngmin Kim <kimyoungmin@gmail.com>
2020-05-06 14:24:40 +0900

    gstreamer rk3399 cross compile

--- cross_compile/c.sh
+++ cross_compile/c.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-/opt/gcc-linaro-7.3.1-2018.05-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc basic-tutorial-1.c --sysroot=/opt/rootfs-rk3399pc-ubuntu18.04 -pthread -I/opt/rootfs-rk3399pc-ubuntu18.04/usr/include/gstreamer-1.0 -I/opt/rootfs-rk3399pc-ubuntu18.04/usr/include/glib-2.0 -I/opt/rootfs-rk3399pc-ubuntu18.04/usr/lib/aarch64-linux-gnu/glib-2.0/include -Wl,-rpath-link,/opt/rootfs-rk3399pc-ubuntu18.04/usr/lib/aarch64-linux-gnu -Wl,-rpath-link,/opt/rootfs-rk3399pc-ubuntu18.04/lib/aarch64-linux-gnu -lgstreamer-1.0 -lgobject-2.0 -lglib-2.0
+SRC=basic-tutorial-1.c
+CC=/opt/gcc-linaro-7.3.1-2018.05-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc 
+SYSROOT=/opt/rootfs-rk3399pc-ubuntu18.04 
 
+$CC --sysroot=$SYSROOT -pthread -I$SYSROOT/usr/include/gstreamer-1.0 -I$SYSROOT/usr/include/glib-2.0 -I$SYSROOT/usr/lib/aarch64-linux-gnu/glib-2.0/include -Wl,-rpath-link,$SYSROOT/usr/lib/aarch64-linux-gnu -Wl,-rpath-link,$SYSROOT/lib/aarch64-linux-gnu -lgstreamer-1.0 -lgobject-2.0 -lglib-2.0 $SRC
