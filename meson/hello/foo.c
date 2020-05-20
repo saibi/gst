8cf10964917e989df84c35ab734f73a141ed8486
Youngmin Kim <kimyoungmin@gmail.com>
2020-05-20 10:25:44 +0900

    meson : multiple src

---
+++ meson/hello/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "foo.h"
+
+void foo(void)
+{
+	printf("I am foo.\n");
+}
--- meson/hello/main.c
+++ meson/hello/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include "foo.h"
 
 int main(int argc, char *argv[])
 {
 	printf("Hello world\n");
+
+	foo();
 
 	return 0;
 
