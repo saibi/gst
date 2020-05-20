b19b917fc080a858c2d81be60ea270f4b50591ce
Youngmin Kim <kimyoungmin@gmail.com>
2020-05-20 11:47:49 +0900

    meson : use obj

---
+++ meson/lib_hello/bar.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void bar(void)
+{
+	printf("I'am bar.\n");
+}
--- meson/lib_hello/main.c
+++ meson/lib_hello/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "foo.h"
 
+extern void bar(void);
+
 int main(int argc, char *argv[])
 {
 	printf("Hello world\n");
 
 	foo();
+	bar();
 
 	return 0;
 
